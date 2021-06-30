bc0f7c143f4453489dc023f53fb90433ba0678af
Luis <luis.espinoza.nav@live.com>
2021-06-30 03:09:51 -0300

    Fish tutorial part 3!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../component/Layout/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
       <main>
           <Layout pageTitle="Home Page">
               <p>I'm making this by following the Gatsby Tutorial.</p>
+              <StaticImage src="../images/perrito.jpg" alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera" />
           </Layout>
       </main>
   )
